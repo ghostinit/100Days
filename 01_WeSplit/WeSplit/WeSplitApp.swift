47dc3b49e5f96d6b0e2c878c7c219c42b33ff218
Adam Lyon <adam@nullrider.com>
2025-05-10 08:37:42 -0600

    Initial Commit

---
+++ 01_WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Adam Lyon on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
